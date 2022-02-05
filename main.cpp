0cf9a95ace647f03627668db9280c895a606f443
Brian Horner <bhorner@suffolk.edu>
2022-02-05 18:17:40 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// CS472 Project 1 - MIPS Disassembler
+//
+// Author: Brian Horner
+// Edit History:
+// 2/5/2022 - Initial Version and Establishing Git Repo
+
+// Standard Library Includes
+#include <iostream>
+
+// User Built Includes
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
